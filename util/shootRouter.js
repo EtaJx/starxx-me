26c04761101dfbd757d9786b3d8fb8cf0a313833
Neras <medianeras57@gmail.com>
2018-12-05 23:51:17 +0800

    shoot api router

--- util/shootRouter.js
+++ util/shootRouter.js
@@ -2,15 +2,19 @@ const fs = require('fs')
 const path = require('path')
 
 const CreateRouter = (options) => {
-  const { next, router } = options
+  const { next, router, routerPath, isNext } = options
   const root = path.join(__dirname, '../')
-  const files = fs.readdirSync(`${root}/pages`)
+  const files = fs.readdirSync(`${root}${routerPath}`)
   for (let file of files) {
     let fileName = path.basename(file, '.js')
-    router.get(`/${fileName}`, async ctx => {
-      await next.render(ctx.req, ctx.res, `/${fileName}`, ctx.query)
-      ctx.response = false
-    })
+    if(isNext) {
+      router.get(`/${fileName}`, async ctx => {
+        await next.render(ctx.req, ctx.res, `/${fileName}`, ctx.query)
+        ctx.response = false
+      })
+    } else {
+      router.use(require(`${root}${routerPath}/${fileName}.js`).routes())
+    }
   }
 }
 
