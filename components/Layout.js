f6f4a02c3cd977500b49b101fcdf8db4cc473b6e
Neras <medianeras57@gmail.com>
2018-12-07 00:07:37 +0800

    optimization

--- components/Layout.js
+++ components/Layout.js
@@ -1,6 +1,9 @@
 import Header from './common/Header'
 import Nav from './common/Nav'
 import { withRouter } from 'next/router'
+import '../lib/prism/prism.js'
+import '../lib/prism/prism.css'
+
 
 import './style.less'
 
--- pages/index.js
+++ pages/index.js
@@ -1,20 +1,32 @@
 import Layout from '../components/Layout'
 import fetch from 'node-fetch'
+import React, { Component } from 'react'
+import '../lib/prism/prism.js'
+import '../lib/prism/prism.css'
 
-const Index = ({ok, articles}) => {
-  return (
-    <Layout>
-      <div dangerouslySetInnerHTML={{__html: articles[0]}}></div>
-    </Layout>
-  )
-}
-
-Index.getInitialProps = async () => {
-  const res = await fetch('http://localhost:8080/api')
-  const json = await res.json()
-  return {
-    ok: json.ok,
-    articles: json.articles
+class Index extends Component {
+  static async getInitialProps({req}) {
+    const res = await fetch(`http://localhost:8080/api`)
+    const json = await res.json()
+    return {
+      ok: json.ok,
+      articles: json.articles
+    }
+  }
+  render() {
+    const { articles } = this.props
+    return (
+      <Layout>
+        <header>
+          <h3 style={{
+            textAlign: 'center'
+          }} dangerouslySetInnerHTML={{ __html: articles[0].header.title }}></h3>
+        </header>
+        <div style={{
+          padding: '0 10px'
+        }} dangerouslySetInnerHTML={{ __html: articles[0].html }}></div>
+      </Layout>
+    )
   }
 }
 
