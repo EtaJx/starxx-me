00462b855fc825541822dd5f31b44330808fee18
Neras <medianeras57@gmail.com>
2018-12-07 00:06:39 +0800

    parse markdown

--- api/index.js
+++ api/index.js
@@ -1,7 +1,14 @@
 const router = new require('koa-router')()
 const fs = require('fs')
 const path = require('path')
-const markdown = require( "markdown" ).markdown
+const cheerio = require('cheerio')
+const yaml2js = require('js-yaml')
+const markdown = require('markdown-it')({
+  html: true,
+  linkify: true,
+  typographer: true,
+  langPrefix:   'language-'
+});
 
 const index = (ctx, next) => {
   const mdPath = path.join(__dirname, '../_post')
@@ -9,7 +16,14 @@ const index = (ctx, next) => {
   let resMds = []
   for(let md of mds) {
     const file = fs.readFileSync(`${mdPath}/${md}`, 'utf8')
-    resMds.push(markdown.toHTML(file))
+    const html = markdown.render(file)
+    const $ = cheerio.load(html)
+    const header = yaml2js.load($('code').first().html())
+    $('code').first().remove()
+    resMds.push({
+      header,
+      html: $.html()
+    })
   }
   ctx.body= {
     ok: true,
