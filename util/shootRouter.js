cd3c50120fc147e1539ff8ddd50a5af87e5ebe86
Neras <medianeras57@gmail.com>
2018-12-06 09:55:13 +0800

    opitimization variable name

--- util/shootRouter.js
+++ util/shootRouter.js
@@ -2,18 +2,20 @@ const fs = require('fs')
 const path = require('path')
 
 const CreateRouter = (options) => {
-  const { next, router, routerPath, isNext } = options
+  const { next, shooter, routerPath, isNext } = options
   const root = path.join(__dirname, '../')
   const files = fs.readdirSync(`${root}${routerPath}`)
   for (let file of files) {
     let fileName = path.basename(file, '.js')
     if(isNext) {
-      router.get(`/${fileName}`, async ctx => {
+      // here shooter is router
+      shooter.get(`/${fileName}`, async ctx => {
         await next.render(ctx.req, ctx.res, `/${fileName}`, ctx.query)
         ctx.response = false
       })
     } else {
-      router.use(require(`${root}${routerPath}/${fileName}.js`).routes())
+      // here shooter is new Koa()
+      shooter.use(require(`${root}${routerPath}/${fileName}.js`).routes())
     }
   }
 }
