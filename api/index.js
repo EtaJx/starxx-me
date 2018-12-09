82713be1947e3472d45021f96b20c04368936512
Neras <medianeras57@gmail.com>
2018-12-09 21:01:53 +0800

    recode api

--- api/index.js
+++ api/index.js
@@ -1,33 +1,13 @@
 const router = new require('koa-router')()
-const fs = require('fs')
-const path = require('path')
-const cheerio = require('cheerio')
-const yaml2js = require('js-yaml')
-const markdown = require('markdown-it')({
-  html: true,
-  linkify: true,
-  typographer: true,
-  langPrefix:   'language-'
-});
+const { parseHtml } = require('../lib/parseHtml')
 
 const index = (ctx, next) => {
-  const mdPath = path.join(__dirname, '../_post')
-  const mds = fs.readdirSync(`${mdPath}`)
-  let resMds = []
-  for(let md of mds) {
-    const file = fs.readFileSync(`${mdPath}/${md}`, 'utf8')
-    const html = markdown.render(file)
-    const $ = cheerio.load(html)
-    const header = yaml2js.load($('code').first().html())
-    $('code').first().remove()
-    resMds.push({
-      header,
-      html: $.html()
-    })
-  }
+  const articles = parseHtml({
+    markdownPath: '_post'
+  })
   ctx.body= {
     ok: true,
-    articles: resMds
+    articles: articles
   }
 }
 
