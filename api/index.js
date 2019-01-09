d202fb4592ad27d0c8f3806d166ac682768d4fe9
Neras <medianeras57@gmail.com>
2019-01-09 23:06:24 +0800

    change api

--- api/index.js
+++ api/index.js
@@ -1,27 +1,28 @@
-const router = new require('koa-router')()
 const { parseHtml } = require('../lib/parseHtml')
 
-const index = (ctx, next) => {
-  const { resMds } = parseHtml({
-    markdownPath: '_post'
-  })
-  ctx.body = {
-    ok: true,
-    articles: resMds
+module.exports = (router) => {
+  const index = (ctx, next) => {
+    const { index } = ctx.query
+    const { resMds } = parseHtml({
+      markdownPath: '_post'
+    })
+    ctx.body = {
+      ok: true,
+      article: resMds[index]
+    }
   }
-}
 
-const articleList = (ctx, next) => {
-  const { listMds } = parseHtml({
-    markdownPath: '_post'
-  })
-  ctx.body = {
-    ok: true,
-    list: listMds
+  const articleList = (ctx, next) => {
+    const { listMds } = parseHtml({
+      markdownPath: '_post'
+    })
+    ctx.body = {
+      ok: true,
+      list: listMds
+    }
   }
+
+  router.get('/api', index)
+  router.get('/list', articleList)
+  return router
 }
-
-router.get('/api', index)
-router.get('/list', articleList)
-
-module.exports = router
