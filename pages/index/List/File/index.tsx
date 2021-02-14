be3781e518a5ec426b5320e9a0fde13abe5cd6aa
Neras <medianeras57@gmail.com>
2021-02-14 15:54:24 +0800

    adapte mobile

--- pages/index/List/File/index.tsx
+++ pages/index/List/File/index.tsx
@@ -9,13 +9,14 @@ type FileProps = {
 }
 const File: React.FC<FileProps> = (props) => {
   const { file = {} } = props;
-  const { name, modifiedTime, id } = file;
+  const { name = '', modifiedTime, id } = file;
+  const adjustName = name.replace('.md', '');
   const localTime = moment(modifiedTime).utc().utcOffset(+8).locale('zh-CN').format('llll');
   return (
     <Link href={`/article/${id}`}>
       <div className="file-wrapper">
-        <span>{name ?? ''}</span>
-        <span>{localTime}</span>
+        <span className="file-name">{adjustName ?? ''}</span>
+        <span className="file-createtime">{localTime}</span>
       </div>
     </Link>
   );
