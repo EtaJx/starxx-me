263283c94615128ca9c586d71b8e992031842f4a
EtaJx <medianeras57@gmail.com>
2022-08-08 15:43:14 +0800

    Update schedule interval

--- lib/schedule.ts
+++ lib/schedule.ts
@@ -1,13 +1,14 @@
 import schedule from 'node-schedule';
-import { initGooleDriverAuthor } from './utils';
+import { initGoogleDriverAuthor } from './utils';
 
 const rule = new schedule.RecurrenceRule();
 rule.second = 0;
-rule.minute = [0, 30];
+rule.minute = 0;
+rule.hour = [0, 6, 12, 18];
 
 export const startSchedule = () => {
   schedule.scheduleJob(rule, async () => {
     console.log('start update list');
-    await initGooleDriverAuthor();
+    await initGoogleDriverAuthor();
   });
 };
